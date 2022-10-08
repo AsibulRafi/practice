35b49580619643ffc7442ef79097ba5146a9fa6d
Asibul Rafi <asibulrafi@gmail.com>
2022-10-09 01:48:05 +0600

    Add Filter method

--- src/context/context.js
+++ src/context/context.js
@@ -50,9 +50,58 @@ export default class RoomProvider extends Component {
     const room = tempRooms.find((room) => room.slug === slug);
     return room;
   };
+  // This get the controlled inputs property from the filter method and update it the state with roomlist
   handleChange = (event) => {
-    const type = event.target.type;
+    const target = event.target;
+    // The if or else statement is for checkbox value
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = event.target.name;
+    this.setState(
+      {
+        // This is update the state to the name and value by the changed filter value
+        [name]: value,
+      },
+      // And It call the filterRooms function
+      this.filterRooms
+    );
   };
+
+  filterRooms = () => {
+    let { rooms, type, capacity, price, minSize, maxSize, breakfast, pets } =
+      this.state;
+
+    let tempRooms = [...rooms];
+    // transform values
+    // get capacity
+    capacity = parseInt(capacity);
+    price = parseInt(price);
+    // filter by type
+    if (type !== "all") {
+      tempRooms = tempRooms.filter((room) => room.type === type);
+    }
+    // filter by capacity
+    if (capacity !== 1) {
+      tempRooms = tempRooms.filter((room) => room.capacity >= capacity);
+    }
+    // filter by price
+    tempRooms = tempRooms.filter((room) => room.price <= price);
+    //filter by size
+    tempRooms = tempRooms.filter(
+      (room) => room.size >= minSize && room.size <= maxSize
+    );
+    //filter by breakfast
+    if (breakfast) {
+      tempRooms = tempRooms.filter((room) => room.breakfast === true);
+    }
+    //filter by pets
+    if (pets) {
+      tempRooms = tempRooms.filter((room) => room.pets === true);
+    }
+    this.setState({
+      sortedRooms: tempRooms,
+    });
+  };
+
   render() {
     return (
       <RoomContext.Provider
